ff04167925e6d3dd49ce2f2afd69bcfe6ad06fc5
bhenrique16 <bh.henrique16@gmail.com>
2023-02-28 01:52:31 -0300

    conexões sqlite3

---
+++ server/src/database/migrations/00_create_users.ts
@@ -0,0 +1,17 @@
+import Knex  from 'knex';
+
+
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('users',table => {
+        table.increments('id').primary();
+        table.increments('name').notNullable();
+        table.increments('avatar').notNullable();
+        table.increments('whatsapp').notNullable();
+        table.increments('bio').notNullable();
+    })
+}
+
+export async function down(knex:Knex){
+        return knex.schema.dropTable('users');
+}
