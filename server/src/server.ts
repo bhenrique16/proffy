10bc72081a2072ba72c9558a97ee385a80ae5091
bhenrique16 <bh.henrique16@gmail.com>
2023-02-28 02:12:29 -0300

    second commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,13 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users',()=>{
-    console.log('Acessou');
+app.use(express.json())
+
+app.get('/',(request,response)=>{
+    
+
+    return response.send({message:'hello'})
+   
     
 })
 app.listen(3333)
